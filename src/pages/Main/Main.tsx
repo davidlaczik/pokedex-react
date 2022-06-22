4597aa179c617a68e718dc29c581fde16d0456a7
Dávid Laczik <david@Davids-MacBook-Air.local>
2022-06-23 01:17:46 +0200

    Update - Main Page
    Update style and layout

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { Header } from 'layouts';
 
+import styles from './Main.module.css';
+
 const Main = () => {
 	return (
 		<React.Fragment>
 			<Header title='Pokédex' />
+			<main className={styles.main}></main>
 		</React.Fragment>
 	);
 };

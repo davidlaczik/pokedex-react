8f0b93a940a478f2fb140c3fb2b76979bc316dca
Dávid Laczik <david@Davids-MacBook-Air.local>
2022-06-24 02:41:37 +0200

    Update - PokedexListItem Component
    Add modal to the component

--- src/components/PokedexListItem/PokedexListItem.tsx
+++ src/components/PokedexListItem/PokedexListItem.tsx
@@ -1,3 +1,6 @@
+import { PokemonDetailModal } from 'modals';
+import React, { useState } from 'react';
+
 import IPokemon from 'types/IPokemon';
 
 import styles from './PokedexListItem.module.css';
@@ -7,32 +10,51 @@ interface IPokedexListItemProps {
 }
 
 const PokedexListItem = (props: IPokedexListItemProps) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const pokemon = props.pokemon;
 
+	const openModalHandler = () => {
+		setIsModalOpen(!isModalOpen);
+	};
+
 	return (
-		<li className={styles.item}>
-			<div className={styles['item__information']}>
-				<div className={styles['information__id']}>{pokemon.id}</div>
-				<div className={styles['information__name']}>
-					{pokemon.name}
-				</div>
-				<div className={styles['information__type']}>
-					{pokemon.types.map((item, index) => (
-						<span key={index} className={styles['type__name']}>
-							{item.type.name}
-						</span>
-					))}
-				</div>
-			</div>
-			<div className={styles['item__image']}>
-				<img
-					src={
-						pokemon.sprites.other['official-artwork'].front_default
-					}
-					alt={pokemon.name}
+		<React.Fragment>
+			{isModalOpen ? (
+				<PokemonDetailModal
+					pokemon={pokemon}
+					onClose={openModalHandler}
 				/>
-			</div>
-		</li>
+			) : (
+				''
+			)}
+			<li className={styles.item} onClick={openModalHandler}>
+				<div className={styles['item__information']}>
+					<div className={styles['information__id']}>
+						{pokemon.id}
+					</div>
+					<div className={styles['information__name']}>
+						{pokemon.name}
+					</div>
+					<div className={styles['information__type']}>
+						{pokemon.types.map((item, index) => (
+							<span key={index} className={styles['type__name']}>
+								{item.type.name}
+							</span>
+						))}
+					</div>
+				</div>
+				<div className={styles['item__image']}>
+					<img
+						src={
+							pokemon.sprites.other['official-artwork']
+								.front_default
+						}
+						alt={pokemon.name}
+					/>
+				</div>
+			</li>
+		</React.Fragment>
 	);
 };
 

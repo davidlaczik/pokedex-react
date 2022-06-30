835e2ae7dacdb8ce89e764d7129a813fbdbbb617
Dávid Laczik <david@Davids-MacBook-Air.local>
2022-06-30 02:12:00 +0200

    Update - PokedexList
    Code Refactor

--- src/components/PokedexList/PokedeList.tsx
+++ src/components/PokedexList/PokedeList.tsx
@@ -1,17 +1,17 @@
-import PokedexListItem from 'components/PokedexListItem';
+import { Pokemon } from 'types';
 
-import IPokemon from 'types/IPokemon';
+import { PokedexListItem } from 'components';
 
 import styles from './PokedexList.module.css';
 
 interface IPokedexListProps {
-	data: IPokemon[];
+	data: Pokemon[];
 }
 
 const PokedexList = (props: IPokedexListProps) => {
 	return (
 		<ul className={styles.list}>
-			{props.data?.map((item, index) => (
+			{props.data.map((item, index) => (
 				<PokedexListItem key={index} pokemon={item} />
 			))}
 		</ul>
